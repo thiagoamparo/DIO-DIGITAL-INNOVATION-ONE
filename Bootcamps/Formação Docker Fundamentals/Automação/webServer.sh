176b977c866bc47e653b11616287c057231d4120
T-Amparo <78854010+T-Amparo@users.noreply.github.com>
2024-05-09 00:22:27 -0300

    Project Challenge - Docker Compose Automation.
    Update of the project challenge README file.
    Creation of the automation file for executing the Web Application using Docker Compose.

---
+++ Bootcamps/Formação Docker Fundamentals/Automação/webServer.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "Iniciando Configurações do Servidor Web..."
+
+echo "Preparando Ambiente Para Execução de Container..."
+
+echo "Parando Containers Em Execução."
+
+docker rm -f Web_Aplication DataBase_Aplication
+
+echo "Limpando Volumes Existentes."
+
+docker volume rm project_www_data project_database_data
+
+echo "Baixando e Salvando Arquivos da Aplicação Web."
+
+cd /tmp
+wget https://github.com/thiagoamparo/DIO-DIGITAL-INNOVATION-ONE/archive/refs/heads/main.zip
+
+echo "Descompactando Aplicação Web"
+
+unzip -o main.zip
+
+echo "Criando Repositorio Para Container."
+
+mkdir /containers
+mkdir /containers/tasks
+
+echo "Movendo Arquivos De Container Para Repositorio De Containers."
+
+cd /tmp/"DIO-DIGITAL-INNOVATION-ONE-main"/"Bootcamps"/"Formação Docker Fundamentals"/"Desafios de Projeto"/"1º Desafio de Projeto"
+cp docker-compose.yml /containers/tasks
+
+echo "Inicializando Container."
+
+cd /containers/tasks
+docker-compose up -d
+
+echo "Movendo Arquivos Da Aplicação Web para Container."
+
+cd /tmp/"DIO-DIGITAL-INNOVATION-ONE-main"/"Bootcamps"/"Formação Docker Fundamentals"/"Aplicação Web"
+cp -R * /var/lib/docker/volumes/tasks_www_data/_data
+
+echo "Limpando Arquivos Baixados."
+
+cd /tmp
+rm main.zip -f
+rm DIO-DIGITAL-INNOVATION-ONE-main -r -d -f
+
+echo "Configuração do Servidor Web Finalizada."
--- Bootcamps/Formação Docker Fundamentals/Desafios de Projeto/1º Desafio de Projeto/README.md
+++ Bootcamps/Formação Docker Fundamentals/Desafios de Projeto/1º Desafio de Projeto/README.md
@@ -1,18 +1,20 @@
 # Docker Compose: Executando uma aplicação HTML em um container apache (HTTPD)
 
-Para este projeto, será utilizada uma máquina virtual Linux Server juntamente com o editor de texto Nano para criar o arquivo `docker-compose.yml` responsável pela criação e execução do `container` que manterá a `Aplicação Web`.
+Para este projeto, será utilizada uma máquina virtual Linux Server juntamente com o editor de texto Nano para criar o arquivo `docker-compose.yml`, responsável pela criação e execução do `container` que hospedará a `Aplicação Web`.
 
 ## Objetivo
 
-Desenvolver o arquivo `docker-compose.yml` responsável pela criação e execução do `container`.
+Desenvolver o arquivo `docker-compose.yml` responsável por criar e gerenciar `container` que hospedará a Aplicação Web.
 
 ## Requisitos
 
-1. Criar arquivo YML.
+1. Criar o arquivo `YML` responsavel pela criação do `container`.
 
-2. Definir as configurações de um servidor `apache (HTTPD)`.
+2. Definir as configurações necessarias para o servidor `apache (HTTPD)` presente no `container`.
 
-3. Especificar local da `Aplicação Web`.
+3. Especificar o local onde a `Aplicação Web` será hospedada no `container`.
+
+4. Configurar o redirecionamento de `portas` para permitir o `acesso` à `Aplicação Web` via navegador.
 
 ## Extra
 
